16d71b40816c65ae87ffdd91c09dbf142a7ff2e4
WolferTamer <wolfertamer@gmail.com>
2026-09-03 11:46:28 -0500

    Implemented up to episode 8

--- src/main/java/com/calebcassin/tutorialmod/TutorialMod.java
+++ src/main/java/com/calebcassin/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package com.calebcassin.tutorialmod;
 
+import com.calebcassin.tutorialmod.block.ModBlocks;
+import com.calebcassin.tutorialmod.item.ModCreativeModeTabs;
 import com.calebcassin.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -34,7 +36,10 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModCreativeModeTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -51,6 +56,11 @@ public class TutorialMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.BISMUTH);
             event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
     }
 
---
+++ src/main/java/com/calebcassin/tutorialmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,44 @@
+package com.calebcassin.tutorialmod.item;
+
+import com.calebcassin.tutorialmod.TutorialMod;
+import com.calebcassin.tutorialmod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> BISMUTH_ITEMS_TAB = CREATIVE_MODE_TAB.register("bismuth_items_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.BISMUTH.get())).title(Component.translatable("creativetab.ccassinnftutorial.bismuth_items"))
+            .displayItems((itemDisplayParameters, output) -> {
+                output.accept(ModItems.BISMUTH);
+                output.accept(ModItems.RAW_BISMUTH);
+                output.accept(ModItems.CHISEL);
+                output.accept(ModItems.RADISH);
+                output.accept(ModItems.STARLIGHT_ASHES);
+                output.accept(ModItems.FROSTFIRE_ICE);
+            }).build());
+
+    public static final Supplier<CreativeModeTab> BISMUTH_BLOCKS_TAB = CREATIVE_MODE_TAB.register("bismuth_blocks_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModBlocks.BISMUTH_BLOCK))
+            .withTabsBefore(ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, "bismuth_items_tab"))
+            .title(Component.translatable("creativetab.ccassinnftutorial.bismuth_blocks"))
+            .displayItems((itemDisplayParameters, output) -> {
+                output.accept(ModBlocks.BISMUTH_BLOCK);
+                output.accept(ModBlocks.BISMUTH_ORE);
+                output.accept(ModBlocks.BISMUTH_DEEPSLATE_ORE);
+                output.accept(ModBlocks.MAGIC_BLOCK);
+            }).build());
+
+
+    public static void register(IEventBus bus) {
+        CREATIVE_MODE_TAB.register(bus);
+    }
+}
